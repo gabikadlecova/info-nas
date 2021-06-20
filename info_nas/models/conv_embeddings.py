3854c5e3f7890ae602d95699c36145606eff7c4e
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2021-06-20 10:22:27 +0200

    Started writing models for io data.

--- info_nas/models/conv_embeddings.py
+++ info_nas/models/conv_embeddings.py
@@ -6,8 +6,9 @@ from abc import ABC, abstractmethod
 #    b) the embedding is the second actual input
 
 
-class IOModel(ABC):
+class IOModel(nn.Module):
     def __init__(self, vae_model):
+        super().__init__()
         self.vae_model = vae_model
 
     @abstractmethod
@@ -22,16 +23,25 @@ class IOModel(ABC):
 
 
 class SimpleConvModel(IOModel):
-    def __init__(self, vae_model, inputs_shape, outputs_shape, n_steps=3, n_convs=2):
+    def __init__(self, vae_model, input_channels, output_channels, n_steps=2, n_convs=2):
         super().__init__(vae_model)
 
+        channels = input_channels
         conv_list = []
-        # TODO outputs first?
-        nn.Conv2d()
 
-        # TODO nějakej np space pro výběr hodnot od in shape do out shape
+        for _ in range(n_steps):
+            for _ in range(n_convs - 1):
+                conv_list.append(nn.Conv2d(channels, channels, 3))
 
+            next_channels = channels // 2
+            conv_list.append(nn.Conv2d(channels, next_channels, 3, stride=2))
+            channels = next_channels
 
+        conv_list.append(nn.Conv2d(channels, output_channels, 1))
+
+        self.conv_list = nn.ModuleList(conv_list)
+        # TODO kam se z?
 
     def inputs_forward(self, z, inputs):
         pass
+        # TODO z shape?
