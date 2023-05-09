Fix bug where prep reverse was not called in loss, leading to wrong training results. Add gradient clipping.

info_nas/datasets/base.py
@@ -83,6 +83,9 @@ class NetworkDataModule(pl.LightningDataModule):
 
     def train_dataloader(self):
         loaders = self._get_loaders('train')
+        if len(loaders) == 1:
+            return next(iter(loaders))
+
         return CombinedLoader(loaders, mode='max_size_cycle')
 
     def val_dataloader(self):
@@ -97,7 +100,7 @@ class NetworkDataModule(pl.LightningDataModule):
 
         shuffle = True if key == 'train' else False
         loaders = apply_to_vals(self.data[key], lambda d: DataLoader(d, batch_size=self.batch_size, shuffle=shuffle))
-        return loaders if len(loaders) > 1 else loaders[0]
+        return loaders
 
 
 class NetworkDataset(torch.utils.data.Dataset):
@@ -134,6 +137,7 @@ def _load_data_or_iterable(data, init_func):
 
 def _not_dict_and_list(data):
     return not isinstance(data, dict) and not isinstance(data, list)
+
 
 def _to_dict_or_list(data, type):
     assert type == dict or type == list

info_nas/metrics/arch2vec.py
@@ -7,16 +7,18 @@ from torchmetrics import Metric
 
 
 class VAELoss(nn.Module):
-    def __init__(self, adj_loss=None, ops_loss=None, w_ops=1.0, w_adj=1.0):
+    def __init__(self, preprocessor, adj_loss=None, ops_loss=None, w_ops=1.0, w_adj=1.0):
         super().__init__()
         self.adj_loss = nn.BCELoss() if adj_loss is None else adj_loss
         self.ops_loss = nn.BCELoss() if ops_loss is None else ops_loss
         self.w_ops = w_ops
         self.w_adj = w_adj
 
+        self.preprocessor = preprocessor
+
     def __call__(self, y_pred, y_true):
-        ops, adj = y_true
-        ops_recon, adj_recon, mu, logvar = y_pred
+        _, _, mu, logvar = y_pred
+        ops, adj, ops_recon, adj_recon = _get_adj_ops(y_pred, y_true, self.preprocessor)
 
         loss_ops = self.ops_loss(ops_recon, ops)
         loss_adj = self.adj_loss(adj_recon, adj)

info_nas/vae_models.py
@@ -1,5 +1,5 @@
 import os.path
-from copy import copy
+from copy import copy, deepcopy
 
 import pytorch_lightning as pl
 import torch
@@ -134,8 +134,8 @@ class NetworkVAE(pl.LightningModule):
 
 
 class InfoNAS(NetworkVAE):
-    def __init__(self, model, labeled_model, loss, labeled_loss, preprocessor, metrics=None, labeled_metrics=None):
-
+    def __init__(self, model, labeled_model, loss, labeled_loss, preprocessor, metrics=None, labeled_metrics=None,
+                 clip=5):
         super().__init__(model, loss, preprocessor, metrics=metrics)
 
         self.labeled_model = labeled_model
@@ -144,6 +144,7 @@ class InfoNAS(NetworkVAE):
         self.labeled_metrics = nn.ModuleDict(labeled_metrics) if labeled_metrics is not None else None
 
         self.automatic_optimization = False
+        self.clip = clip
 
     def training_step(self, batch, batch_idx):
         batch, labeled_batch = batch['unlabeled'], batch['labeled']
@@ -154,12 +155,14 @@ class InfoNAS(NetworkVAE):
         loss = self.compute_loss(batch, batch_idx, 'train', prog_bar=True)
         self.log(f'train/unlabeled_loss', loss)
         self.manual_backward(loss)
+        self.clip_gradients(opt, self.clip)
         opt.step()
 
         # labeled step
         labeled_opt.zero_grad()
         loss = self.compute_loss(labeled_batch, batch_idx, 'train', prog_bar=True)
         self.manual_backward(loss)
+        self.clip_gradients(labeled_opt, self.clip)
         labeled_opt.step()
 
     def save_model_args(self, dir_path):
@@ -233,7 +236,7 @@ class InfoNAS(NetworkVAE):
 
 
 def _init_loss(loss):
-    return {'train': loss, 'val': copy(loss), 'test': copy(loss)}
+    return {'train': loss, 'val': deepcopy(loss), 'test': deepcopy(loss)}
 
 
 def _save_model(dir_path, model, model_name):